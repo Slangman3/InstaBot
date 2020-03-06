1c40f3254e9658535df22cf277d5c2fac3b39465
slangman3 <slangman3@gmail.com>
2020-03-06 18:35:12 +0300

    bot v0.1

---
+++ src/main/java/Instalike.java
@@ -0,0 +1,220 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.util.Map.entry;
+
+class Instalike {
+
+    private final Map<String, String> xpaths = Map.ofEntries(
+            entry("Log_in", "//*[contains(text(), 'Log In')]"),
+            entry("Subscribe", "//*[text() = 'Follow']"),
+            entry("Subscribed", "//*[text() = 'Following']"),
+            entry("First_photo", "//*[@id='react-root']/section/main/div/div[2]/article/div[1]/div/div[1]/div[1]/a"),
+            entry("Like_button", "/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button"),
+            entry("Next_button_first", "//*[@id='react-root']/html/body/div[3]/div/div[1]/div/div/a"),
+            entry("Next_button_default", "//*[@id='react-root']/html/body/div[3]/div/div[1]/div/div/a[2]"),
+            entry("Next_button_last", "//*[@id='react-root']/html/body/div[3]/div/div[1]/div/div/a")
+    );
+    private final String login;
+    private final String password;
+    private final String target;
+    private ChromeDriver browser;
+    private int baseDelay;
+    private Logger logger;
+
+    Instalike(String login, String password,
+              String target) {
+        this(login, password, target, 3);
+    }
+
+    Instalike(String login, String password,
+              String target, int baseDelay) {
+        this.login = login;
+        this.password = password;
+        this.target = target;
+        this.baseDelay = baseDelay;
+        logger = Logger.getLogger(Instalike.class.getName());
+    }
+
+    void start() {
+        log("InstaBot начал свою работу!");
+        browser = new ChromeDriver();
+        login();
+        follow(30);
+//        subscribe();
+        findFirst();
+        likeCurrent();
+        getNext();
+        likeCurrent();
+//        end();
+    }
+
+    private void login() {
+        browser.get("https://www.instagram.com/accounts/login/");
+        waitASecond();
+        browser.findElement(new By.ByName("username")).sendKeys(login);
+        waitASecond();
+        browser.findElement(new By.ByName("password")).sendKeys(password);
+        browser.findElement(new By.ByXPath(xpaths.get("Log_in"))).click();
+        waitASecond();
+        log(String.format("Залогинились, как %s", login));
+    }
+
+    private boolean follow(int n) {
+        browser.get(String.format("https://www.instagram.com/%s", target));
+        log(String.format("Зашли на страничку к %s", target));
+        waitASecond();
+        browser.findElement(By.xpath(String.format("//*[@href='/%s/followers/']", target))).click();
+        log(String.format("Открыли список фоловеров %s", target));
+        waitASecond();
+//        browser.findElement(By.xpath("/html/body/div[4]/div/div[2]/ul/div/li[1]/div/div[1]/div[1]")).sendKeys(Keys.END);
+//        waitASecond();
+
+
+//        for (int second = 0;; second++) {
+//
+//            if(second >=60){
+//                break;
+//            }
+//            browser.findElement(By.xpath("/html/body/div[4]/div/div[2]/ul/div/li[1]/div/div[1]/div[1]")).sendKeys(Keys.END);
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+
+
+
+//        browser.findElements(By.xpath("/html/body/div[4]/div/div[2]/ul/div/li[1]/div/div[1]/div[1]"));
+//
+//        waitASecond();
+//        browser.navigate().back();
+//        waitASecond();
+//        waitASecond();
+//        ArrayList followersList = new ArrayList();
+//        Object[] list = browser.findElements(By.xpath("/html/body/div[4]/div/div[2]/ul/div/li/div/div[1]/div[1]")).toArray();
+
+        for (int i = 1; i < n; i++) {
+            browser.findElement(By.xpath(String.format("/html/body/div[4]/div/div[2]/ul/div/li[%s]/div/div[1]/div[1]", i))).click();
+            waitASecond();
+            String userName = browser.findElement(By.xpath("//*[@id='react-root']/section/main/div/header/section/div[1]/h1")).getText();
+            log(String.format("Подписываемся на %s.", userName));
+            try {
+                browser.findElement(new By.ByXPath(xpaths.get("Subscribe"))).click();
+                waitASecond();
+
+                return true;
+            } catch (NoSuchElementException ex) {
+                warn("Уже был подписан!");
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean subscribe() {
+        browser.get(String.format("https://www.instagram.com/%s", target));
+        waitASecond();
+        log(String.format("Подписываемся на %s.", target));
+        try {
+            browser.findElement(new By.ByXPath(xpaths.get("Subscribe"))).click();
+            waitASecond();
+            return true;
+        } catch (NoSuchElementException ex) {
+            warn("Уже был подписан!");
+            return false;
+        }
+    }
+
+    private boolean findFirst() {
+        String userName = browser.findElement(By.xpath("//*[@id='react-root']/section/main/div/header/section/div[1]/h1")).getText();
+        log(String.format("Ставим лойсы для %s.", userName));
+        waitASecond();
+        try {
+            browser.findElement(new By.ByXPath(xpaths.get("First_photo"))).isDisplayed();
+            browser.findElement(new By.ByXPath(xpaths.get("First_photo"))).click();
+            waitASecond();
+            return true;
+        } catch (NoSuchElementException ex) {
+            warn("Не могу найти первое фото!");
+            return false;
+        }
+    }
+
+    /**
+     * Лайк-кодим открытую фотографию.
+     */
+    private void likeCurrent() {
+        var likeButton = browser.findElement(new By.ByXPath(xpaths.get("Like_button")));
+        try {
+            var param = browser.findElement(By.xpath("//*[@aria-label='Unlike']"));
+            log("Тут лойс уже стоит!");
+        } catch (NoSuchElementException ex) {
+            likeButton.click();
+            log("Лойс поставлен!");
+            waitASecond();
+        }
+    }
+
+    /**
+     * Открываем следующую фототграфию.
+     *
+     * @return True, если удалось открыть, false в противном случае.
+     */
+    private boolean getNext() {
+        try {
+            browser.findElement(new By.ByClassName("coreSpriteRightPaginationArrow")).click();
+            waitASecond();
+            return true;
+        } catch (NoSuchElementException e) {
+            warn("Не могу открыть следующее фото. Кажется, закончились.");
+            return false;
+        }
+    }
+
+    /**
+     * Заканчиваем работу браузера.
+     */
+    private void end() {
+        browser.close();
+        log("Сегодня поработал!");
+    }
+
+    /**
+     * Ждём секунду. На самом деле от трёх до пяти, случайным образом решается.
+     */
+    private void waitASecond() {
+        try {
+            Thread.sleep((baseDelay + ThreadLocalRandom.current().nextInt(1, 3)) * 1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Логируем сообщение.
+     *
+     * @param message Текст сообщения.
+     */
+    private void log(String message) {
+        logger.log(Level.INFO, String.format("Instalike: %s", message));
+    }
+
+    /**
+     * Логируем предупреждение.
+     *
+     * @param message Текст предупреждения.
+     */
+    private void warn(String message) {
+        logger.log(Level.WARNING, String.format("Instalike: %s", message));
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+
+public class Main {
+
+    private static CredentialProperties credentials;
+
+    public static void main(String[] args) {
+        setup();
+        System.out.println("Запуск бота...");
+        var bot = new Instalike(
+                credentials.getLogin(),
+                credentials.getPassword(),
+                "mentoss_2309"
+        );
+        bot.start();
+    }
+
+    private static void setup() {
+
+        System.setProperty(
+                "java.util.logging.SimpleFormatter.format",
+                "%1$tF %1$tT [%4$s]: %5$s%6$s%n"
+        );
+
+        try {
+            credentials = new CredentialProperties();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "C:\\Users\\ilyavoronovich\\IdeaProjects\\InstaBot\\src\\main\\driver\\chromedriver.exe"
+        );
+    }
+}
